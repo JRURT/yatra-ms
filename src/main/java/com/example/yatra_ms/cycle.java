7b6f2eb4994f080e46f44bd19b111959afb685f9
JRURT <jrurtcare@gmail.com>
2024-06-27 13:43:45 +0530

    codeRefactor

---
+++ src/main/java/com/example/yatra_ms/cycle.java
@@ -0,0 +1,10 @@
+package com.example.yatra_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class cycle {
+    @GetMapping("/cycle")
+    public String getData() {return  "Please book your cycle ticket from yatra app on 10% discount" ; }
+}
